93a6023784dcf1bb3f5c73a539b983a5e032a0f8
Babatunde Lamidi <odunayohlamidi@gmail.com>
2019-12-31 09:38:26 +0100

    🎉chore: firebase init

--- api/pauline.js
+++ api/pauline.js
@@ -247,22 +247,6 @@ const pauline_prayers = [
 
 
     }
-
-    // {
-    //     Book: "Romans",
-    //     Chapter: 1,
-    //     Verses: '8 - 10',
-    //     Text: "First, I thank my God through Jesus Christ for all of you, because your faith is being reported all over the world. God, whom I serve with my whole heart in preaching the gospel of his Son, is my witness how constantly I remember you in my prayers at all times; and I pray that now at last by God’s will the way may be opened for me to come to you."
-
-    // },
-    // {
-    //     Book: "Romans",
-    //     Chapter: 10,
-    //     Verses: 1,
-    //     Text: "Brothers, my heart’s desire and prayer to God for the Israelites is that they may be saved."
-
-    // }
-
 ]
 
 router.get( '/', ( request, response ) =>
